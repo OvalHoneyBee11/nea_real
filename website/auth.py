3a0dd221b4d0fa58264929667bac4f8a77570b66
Lawrence Keen <lawrence1keen@gmail.com>
2025-08-19 18:39:56 +0100

    Authentication, signing in etc

--- website/auth.py
+++ website/auth.py
@@ -1,18 +1,32 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, logout_user, login_required, current_user
 
 auth = Blueprint("auth", __name__)
 
 
 @auth.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template(
-        "login.html",
-    )
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            flash("Login successful!", category="success")
+            login_user(user, remember=True)
+            return redirect(url_for("views.home"))
+        else:
+            flash("Login failed. Check your username and password.", category="error")
+    return render_template("login.html", user=current_user)
 
 
 @auth.route("/logout")
+@login_required
 def logout():
-    return "<p>Logout Page</p>"
+    logout_user()
+    return redirect(url_for("auth.login"))
 
 
 @auth.route("/sign-up", methods=["GET", "POST"])
@@ -21,6 +35,12 @@ def sign_up():
         username = request.form.get("username")
         password1 = request.form.get("password1")
         password2 = request.form.get("password2")
+
+        user = User.query.filter_by(username=username).first()
+        if user:
+            flash("Username already exists!", category="error")
+            return render_template("sign_up.html")
+
         if password1 != password2:
             flash("Passwords do not match!", category="error")
         if len(username) < 3:
@@ -28,6 +48,14 @@ def sign_up():
         if len(password1) < 6:
             flash("Password must be at least 6 characters long!", category="error")
         else:
+            new_user = User(
+                username=username,
+                password=generate_password_hash(password1, method="pbkdf2:sha256"),
+            )
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(user, remember=True)
             flash("Account created successfully!", category="success")
+            return redirect(url_for("views.home"))
 
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
--- website/models.py
+++ website/models.py
@@ -2,10 +2,15 @@ from . import db
 from flask_login import UserMixin
 
 
+class Tasks(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(1500))
+    date = db.Column(db.DateTime(timezone=True), default=db.func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
-
-    def __repr__(self):
-        return f"<User {self.username}>"
+    tasks = db.relationship("Tasks")
